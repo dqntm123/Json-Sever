bfe4b03e8823baba7afef6aa824b8de49f926109
dqntm123 <36214326+dqntm123@users.noreply.github.com>
2018-04-12 17:07:45 +0900

    20180412
    20180412

---
+++ Assets/Scripts/GetImgFromURL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetImgFromURL : MonoBehaviour {
+
+    public string url = "http://images.earthcam.com/ec_metros/ourcams/fridays.jpg";
+
+    void Start ()
+    {
+        Debug.Log(Application.internetReachability);//인터넷이 연결되있는지 안되있는지 확인할수 있음
+        StartCoroutine(GetIMG());
+	}
+
+    IEnumerator GetIMG()
+    {
+        WWW www = new WWW(url);
+        yield return www;
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.material.mainTexture = www.texture;
+    }
+}
---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MiniJSON;
+using SimpleJSON;
+
+public class Login : MonoBehaviour {
+
+    public string verCheckURL = "";
+	public string gameServerURL ="";
+    public GameObject loginBTN;
+    public GameObject popUpWindow;
+    public UILabel popUpWindowText;
+
+    void Start()
+    {
+        if (Application.internetReachability == 0)
+        {
+            popUpWindow.SetActive(true);
+            popUpWindowText.GetComponent<UILabel>().text = "Could Not Connect Internet";
+            Debug.Log("인터넷 연결 상태를 확인하세요");
+        }
+        else
+        {
+            StartCoroutine(VerCheck());
+        }
+    }
+
+    IEnumerator VerCheck()
+    {
+        WWW www = new WWW(verCheckURL);
+        yield return www;
+        if(www.text == Application.version)
+        {
+            loginBTN.SetActive(true);
+        }
+        else
+        {
+            popUpWindow.SetActive(true);
+            popUpWindowText.GetComponent<UILabel>().text = "Version is not Match";
+        }
+    }
+
+    IEnumerator StartLogin()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("UserId", PlayerPrefs.GetInt("UserID"));
+        form.AddField("UUID", SystemInfo.deviceUniqueIdentifier);
+        WWW www = new WWW(gameServerURL,form);
+        yield return www;
+        Debug.Log(www.text);
+    }
+
+    public void OpenMarket()
+    {
+        Application.OpenURL("https://play.google.com/store/apps/details?id=com.zabob.infinitydungeon2");
+    }
+    public void LoginBtn()
+    {
+        StartCoroutine(StartLogin());
+    }
+}
